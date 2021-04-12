db8e9aa2ae9add5fcdb530223d418cb020129673
Rabbit117 <rkimcho343@gmail.com>
2021-04-12 15:15:06 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdint.h>
+
+extern "C" double royal();
+
+int main(int argc, char* argv[]){
+    double noble=0.0;
+
+    printf();
+    noble = royal();
+    printf();
+    printf();
+
+    return 0;
+}
